c8322c43ea1e662b1c7b68aac07bb620b12a634d
biswajit <biswajip@thoughtworks.com>
2024-05-16 08:04:04 +0530

    NOJIRA| Biswajit| complete first test use jsonpath to validate the response body move the base url assignment to before class as it will be same for the test class

--- src/test/java/com/reqres/demo/getexample/GetUserResource.java
+++ src/test/java/com/reqres/demo/getexample/GetUserResource.java
@@ -1,17 +1,38 @@
 package com.reqres.demo.getexample;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 @Test
 public class GetUserResource {
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://reqres.in/api";
+    }
+
     @Test
     public void shouldListAllUsersFromSpecifiedPageInPagination() {
         given()
                 .when()
-                .get("https://reqres.in/api/users?page=2")
+                .get("/users?page=2")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("page", equalTo(2))
+                .body("per_page", equalTo(6))
+                .body("total", equalTo(12))
+                .body("data.size()", equalTo(6))
+                .body("data[0]", hasKey("id"))
+                .body("data[0]", hasKey("email"))
+                .body("data[0]", hasKey("first_name"))
+                .body("data[0]", hasKey("last_name"))
+                .body("data[0]", hasKey("avatar"))
+                .body("data.findAll {it.keySet().size() != 5}.size()", equalTo(0));
     }
+
+
 }
